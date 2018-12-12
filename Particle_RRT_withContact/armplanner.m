62cc983fb2ce5f0f8e6b25c84925f47352fb2eac
SaumyaSaxena <31939108+SaumyaSaxena@users.noreply.github.com>
2018-12-12 16:07:25 -0500

    Delete armplanner.m

--- Particle_RRT_withContact/armplanner.m
+++
@@ -1,6 +0,0 @@
-function[armplan, armplanlength, numOfParticles] = armplanner(envmap, armstart, armgoal)
-
-
- %call the planner in C
- [armplan, armplanlength, numOfParticles] = particle_RRT(envmap, armstart, armgoal);
-
